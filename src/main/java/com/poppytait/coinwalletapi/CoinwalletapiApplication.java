063699dff7a8b9b5c90dd5205d6ce4808f1d9774
Poppy Tait <poppytait@gmail.com>
2021-05-26 19:26:15 +0200

    Initial commit

---
+++ src/main/java/com/poppytait/coinwalletapi/CoinwalletapiApplication.java
@@ -0,0 +1,13 @@
+package com.poppytait.coinwalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinwalletapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinwalletapiApplication.class, args);
+	}
+
+}
